c2c2ca3ebd20318a05e7a9b020eae280461f0a57
José David Fernández Curado <david.curado@gmail.com>
2015-03-07 16:23:48 -0300

    Add utils

---
+++ src/timetoken/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*- 
+'''
+Created on Mar 7, 2015
+@author: José David Fernández Curado
+'''
+
+def includeme(config):
+    pass
--- src/timetoken/utils.py
+++ src/timetoken/utils.py
@@ -1,4 +1,22 @@
+# -*- coding: utf-8 -*- 
 '''
 Created on Mar 7, 2015
 @author: José David Fernández Curado
 '''
+
+elements = '0123456789abcdefghijklmnopqrstuvwxyz'
+
+def int_to_base36(num):
+    parts = []
+    if num == 0:
+        parts.append(elements[0])
+    while num > 0:
+        parts.append(elements[num % 36])
+        num /= 36
+    parts.reverse()
+    return ''.join(parts)
+def base36_to_int(num):
+    val = 0
+    for i in range(0, len(num)):
+        val = elements.index(num[i]) + val * 36
+    return val
